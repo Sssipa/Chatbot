77eedee04388f3b57b2ad26156a5da1099467c28
Sssipa <sif.asahira1a@gmail.com>
2025-04-27 09:07:04 +0800

    api

---
+++ start/routes.ts
@@ -0,0 +1,31 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+
+router.on('/').render('home')
+router.get('/chatbot', async ({ view }: { view: any }) => {
+    return view.render('chatbot')
+}).as('chatbot')
+
+router.get('/edukasi/mitos-vs-fakta', async ({ view }: { view: any }) => {
+    return view.render('edukasi/mitos')
+}).as('edukasi.mitos')
+
+router.get('/edukasi/artikel', async ({ view }: { view: any }) => {
+    return view.render('edukasi/artikel')
+}).as('edukasi.artikel')
+
+router.get('/edukasi/forum', async ({ view }: { view: any }) => {
+    return view.render('edukasi/forum')
+}).as('edukasi.forum')
+
+router.get('/tentang', async ({ view }: { view: any }) => {
+    return view.render('tentang')
+}).as('tentang')
